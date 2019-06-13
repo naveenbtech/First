61f74aabdb40d9ff1d0e76b79a9b833d65894a3b
Combinedhost <48012688+Combinedhost@users.noreply.github.com>
2019-06-13 21:08:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nk47naveen/daily_learn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nk47naveen.daily_learn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
